49e2910301d624fe21267a12f2a38facce8bee7e
jojomo96 <48690914+jojomo96@users.noreply.github.com>
2024-12-09 00:21:29 +0100

    finish part day one part 1

--- day-1/main.lua
+++ day-1/main.lua
@@ -2,6 +2,7 @@
 
 local list1 = {}
 local list2 = {}
+local differences = {}
 
 
 -- Read input from file
@@ -22,18 +23,45 @@ local function split_pair_of_numbers(line)
     return tonumber(a), tonumber(b)
 end
 
-local input = read_input("testinput.txt")
+-- Function to insert a number while keeping the list sorted
+local function insert_sorted(list, value)
+    local inserted = false
+    for i = 1, #list do
+        if value < list[i] then
+            table.insert(list, i, value)
+            inserted = true
+            break
+        end
+    end
+    if not inserted then
+        table.insert(list, value)
+    end
+end
+
+local input = read_input("input.txt")
 
 for line in input:gmatch("[^\n]+") do
     local a, b = split_pair_of_numbers(line)
     if not a or not b then
         print("Error: Invalid input")
     else 
-        table.insert(list1, a)
-        table.insert(list2, b)
+        insert_sorted(list1, a)
+        insert_sorted(list2, b)
     end
 end
 
--- Print the results
-print("List 1 (First numbers):", table.concat(list1, ", "))
-print("List 2 (Second numbers):", table.concat(list2, ", "))
+for i = 1, #list1 do
+    local difference = list2[i] - list1[i]
+    if difference < 0 then 
+        difference = difference * -1
+    end
+    table.insert(differences, difference)
+end
+
+local sum = 0
+
+for i = 1, #differences do
+    sum = sum + differences[i]
+end
+
+print("Sum of differences: " .. sum)
